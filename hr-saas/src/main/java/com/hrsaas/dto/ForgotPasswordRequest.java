a36ef295655e5dd1f3e458e877f1de4919b3793f
Agbaje Stephen <agbajestephen5@gmail.com>
2026-07-28 12:01:20 +0100

    Create ForgotPasswordRequest.java

---
+++ hr-saas/src/main/java/com/hrsaas/dto/ForgotPasswordRequest.java
@@ -0,0 +1,18 @@
+package com.hrsaas.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ForgotPasswordRequest {
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String companySlug;
+}
