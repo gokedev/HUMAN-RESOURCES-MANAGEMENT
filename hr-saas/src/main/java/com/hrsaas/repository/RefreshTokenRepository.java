7d036707c4726da53b83ff27f9f100d761bc1ce0
Agbaje Stephen <agbajestephen5@gmail.com>
2026-07-28 12:02:25 +0100

    Create RefreshTokenRepository.java

---
+++ hr-saas/src/main/java/com/hrsaas/repository/RefreshTokenRepository.java
@@ -0,0 +1,12 @@
+package com.hrsaas.repository;
+
+import com.hrsaas.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
+    Optional<RefreshToken> findByToken(String token);
+    void deleteByUserId(UUID userId);
+}
