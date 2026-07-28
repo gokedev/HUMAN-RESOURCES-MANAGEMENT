afd3255efc16b38641cb200998445321024150b2
Agbaje Stephen <agbajestephen5@gmail.com>
2026-07-28 12:01:55 +0100

    Create ApiException.java

---
+++ hr-saas/src/main/java/com/hrsaas/exception/ApiException.java
@@ -0,0 +1,35 @@
+package com.hrsaas.exception;
+
+import org.springframework.http.HttpStatus;
+import lombok.Getter;
+
+@Getter
+public class ApiException extends RuntimeException {
+
+    private final HttpStatus status;
+
+    public ApiException(String message, HttpStatus status) {
+        super(message);
+        this.status = status;
+    }
+
+    public static ApiException notFound(String message) {
+        return new ApiException(message, HttpStatus.NOT_FOUND);
+    }
+
+    public static ApiException badRequest(String message) {
+        return new ApiException(message, HttpStatus.BAD_REQUEST);
+    }
+
+    public static ApiException conflict(String message) {
+        return new ApiException(message, HttpStatus.CONFLICT);
+    }
+
+    public static ApiException unauthorized(String message) {
+        return new ApiException(message, HttpStatus.UNAUTHORIZED);
+    }
+
+    public static ApiException forbidden(String message) {
+        return new ApiException(message, HttpStatus.FORBIDDEN);
+    }
+}
