b8fd34e052b5bbdea3d031237b2a8d98aa12af20
Agbaje Stephen <agbajestephen5@gmail.com>
2026-08-04 20:16:08 +0100

    Delete profile.service.ts

--- Frontend/src/services/profile.service.ts
+++
@@ -1,8 +0,0 @@
-import { apiClient } from './axios';
-import type { User } from '../types/api';
-
-export const profileService = {
-  me() {
-    return apiClient.get<User>('/api/employee/me').then((response) => response.data);
-  },
-};
