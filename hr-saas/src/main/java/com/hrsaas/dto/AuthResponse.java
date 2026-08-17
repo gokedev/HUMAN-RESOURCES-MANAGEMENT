36faf2109d2ed1c1ffa225f67257f31878f454ec
Agbaje Stephen <agbajestephen5@gmail.com>
2026-08-17 20:51:34 +0100

    Update AuthResponse.java

--- hr-saas/src/main/java/com/hrsaas/dto/AuthResponse.java
+++ hr-saas/src/main/java/com/hrsaas/dto/AuthResponse.java
@@ -3,9 +3,13 @@ package com.hrsaas.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
 public class AuthResponse {
     private String accessToken;
