8da6b1b64da61c100acfc46e82d064b124c769a7
Agbaje Stephen <agbajestephen5@gmail.com>
2026-08-17 20:52:21 +0100

    Update ErrorResponse.java

--- hr-saas/src/main/java/com/hrsaas/exception/ErrorResponse.java
+++ hr-saas/src/main/java/com/hrsaas/exception/ErrorResponse.java
@@ -1,14 +1,25 @@
 package com.hrsaas.exception;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Getter
+@Setter
+@NoArgsConstructor
 @AllArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ErrorResponse {
     private int status;
+    private String error;
     private String message;
+    private String path;
+    private String code;
     private LocalDateTime timestamp;
 }
