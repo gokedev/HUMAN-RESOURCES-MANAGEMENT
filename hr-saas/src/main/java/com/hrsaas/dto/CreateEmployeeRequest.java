2cec757c02bf2587855dfe4314ce3dd583eaa48b
Agbaje Stephen <agbajestephen5@gmail.com>
2026-08-17 20:51:36 +0100

    Update CreateEmployeeRequest.java

--- hr-saas/src/main/java/com/hrsaas/dto/CreateEmployeeRequest.java
+++ hr-saas/src/main/java/com/hrsaas/dto/CreateEmployeeRequest.java
@@ -2,6 +2,7 @@ package com.hrsaas.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,16 +15,21 @@ public class CreateEmployeeRequest {
 
     @NotBlank
     @Email
+    @Size(max = 200)
     private String email;
 
     @NotBlank
+    @Size(max = 100)
     private String firstName;
 
     @NotBlank
+    @Size(max = 100)
     private String lastName;
 
+    @Size(max = 30)
     private String phone;
 
+    @Size(max = 120)
     private String jobTitle;
 
     private UUID departmentId;
