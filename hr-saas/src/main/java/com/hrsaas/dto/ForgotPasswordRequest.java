6a3bb5854ff95bd282d6684ea0189beab6ee4e62
Agbaje Stephen <agbajestephen5@gmail.com>
2026-08-17 20:51:40 +0100

    Update ForgotPasswordRequest.java

--- hr-saas/src/main/java/com/hrsaas/dto/ForgotPasswordRequest.java
+++ hr-saas/src/main/java/com/hrsaas/dto/ForgotPasswordRequest.java
@@ -2,6 +2,7 @@ package com.hrsaas.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,8 +12,10 @@ public class ForgotPasswordRequest {
 
     @NotBlank
     @Email
+    @Size(max = 200)
     private String email;
 
     @NotBlank
+    @Size(max = 100)
     private String companySlug;
 }
