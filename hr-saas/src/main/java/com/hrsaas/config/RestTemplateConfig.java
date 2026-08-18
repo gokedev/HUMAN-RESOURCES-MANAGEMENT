7a65f59b2a08e0d7bde08ebd142e900c28e3c253
Agbaje Stephen <agbajestephen5@gmail.com>
2026-08-18 21:52:56 +0100

    Update RestTemplateConfig.java

--- hr-saas/src/main/java/com/hrsaas/config/RestTemplateConfig.java
+++ hr-saas/src/main/java/com/hrsaas/config/RestTemplateConfig.java
@@ -2,7 +2,6 @@ package com.hrsaas.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -10,11 +9,7 @@ public class RestTemplateConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
-        factory.setConnectionRequestTimeout(5000); // 5 seconds
-        factory.setConnectTimeout(5000); // 5 seconds
-        factory.setSocketTimeout(5000); // 5 seconds
-        return new RestTemplate(factory);
+        return new RestTemplate();
     }
 
 }
