ae195dcaede03149714bd2a4d7ec5ac6c4507d83
Agbaje Stephen <agbajestephen5@gmail.com>
2026-08-19 11:40:38 +0100

    Update LeaveRequestsPage.jsx

--- Frontend/src/features/leave/LeaveRequestsPage.jsx
+++ Frontend/src/features/leave/LeaveRequestsPage.jsx
@@ -7,7 +7,7 @@ import { DataTableShell, EmptyState, TableSkeleton } from "../../components/feed
 import { Table, TableHeader, TableBody, TableRow, TableHead, TableCell } from "../../components/ui/table.jsx";
 import { useEmployeeNameMap, usePageTitle } from "../../hooks.js";
 import { useToast } from "../../contexts.jsx";
-import { employeeService, leaveService } from "../../api.js";
+import { employeeService, leaveService } from "../../api/index.js";
 import { queryKeys } from "../../constants.js";
 import { getErrorMessage, queryInvalidation } from "../../utils.js";
 import { LeaveDetailsModal, ReviewLeaveModal } from "./LeaveModals.jsx";
