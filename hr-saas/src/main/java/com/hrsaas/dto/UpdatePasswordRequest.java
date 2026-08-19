d058cd04f209abac56a38bb8216e04237b9f536f
Agbaje Stephen <agbajestephen5@gmail.com>
2026-08-19 11:15:14 +0100

    Update UpdatePasswordRequest.java

--- hr-saas/src/main/java/com/hrsaas/dto/UpdatePasswordRequest.java
+++ hr-saas/src/main/java/com/hrsaas/dto/UpdatePasswordRequest.java
@@ -1,6 +1,8 @@
 package com.hrsaas.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,5 +15,10 @@ public class UpdatePasswordRequest {
     private String currentPassword;
 
     @NotBlank
+    @Size(min = 8, max = 128, message = "Password must be between 8 and 128 characters")
+    @Pattern(
+        regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&#])[A-Za-z\\d@$!%*?&#]{8,128}$",
+        message = "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character (@$!%*?&#)"
+    )
     private String newPassword;
 }
