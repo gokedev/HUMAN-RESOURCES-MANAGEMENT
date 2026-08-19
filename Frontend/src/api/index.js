3faddc587b6cf64450c9643b66581d9346b46e80
Agbaje Stephen <agbajestephen5@gmail.com>
2026-08-19 11:40:14 +0100

    Create index.js

---
+++ Frontend/src/api/index.js
@@ -0,0 +1,8 @@
+export { apiClient, setUnauthorizedHandler } from "./client.js";
+export { authService } from "./auth.js";
+export { employeeService } from "./employees.js";
+export { departmentService } from "./departments.js";
+export { attendanceService } from "./attendance.js";
+export { leaveService } from "./leave.js";
+export { profileService } from "./profile.js";
+export { payrollService } from "./payroll.js";
