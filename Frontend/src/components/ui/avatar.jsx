03a478b49c38d091c8704d7bd7e0fd20a3a3e09b
Agbaje Stephen <agbajestephen5@gmail.com>
2026-08-19 03:24:32 +0100

    Update avatar.jsx

--- Frontend/src/components/ui/avatar.jsx
+++ Frontend/src/components/ui/avatar.jsx
@@ -1,6 +1,28 @@
 import * as React from "react";
 import { cn } from "@/lib/utils";
 
+const AVATAR_COLORS = [
+  "from-rose-500 to-pink-600",
+  "from-violet-500 to-purple-600",
+  "from-blue-500 to-indigo-600",
+  "from-cyan-500 to-teal-600",
+  "from-emerald-500 to-green-600",
+  "from-amber-500 to-orange-600",
+  "from-red-500 to-rose-600",
+  "from-fuchsia-500 to-pink-600",
+  "from-sky-500 to-blue-600",
+  "from-lime-500 to-green-600",
+];
+
+function getDeterministicColor(name) {
+  if (!name) return AVATAR_COLORS[0];
+  let hash = 0;
+  for (let i = 0; i < name.length; i++) {
+    hash = name.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  return AVATAR_COLORS[Math.abs(hash) % AVATAR_COLORS.length];
+}
+
 const Avatar = React.forwardRef(({ className, ...props }, ref) => (
   <div
     ref={ref}
@@ -19,15 +41,22 @@ const AvatarFallback = React.forwardRef(({ className, ...props }, ref) => (
 ));
 AvatarFallback.displayName = "AvatarFallback";
 
-const AvatarGradient = React.forwardRef(({ className, children, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn("relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full bg-gradient-to-br from-coral to-coral-strong text-white font-bold items-center justify-center", className)}
-    {...props}
-  >
-    {children}
-  </div>
-));
+const AvatarGradient = React.forwardRef(({ className, children, name, ...props }, ref) => {
+  const gradient = getDeterministicColor(name);
+  return (
+    <div
+      ref={ref}
+      className={cn(
+        "relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full bg-gradient-to-br text-white font-bold items-center justify-center",
+        gradient,
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+});
 AvatarGradient.displayName = "AvatarGradient";
 
-export { Avatar, AvatarFallback, AvatarGradient };
+export { Avatar, AvatarFallback, AvatarGradient, getDeterministicColor };
